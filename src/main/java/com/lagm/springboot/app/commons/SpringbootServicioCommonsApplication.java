417258a26e8044ccfcaef9bb2808fec4d6b361a9
Luis Guisado <lguisadom@gmail.com>
2021-12-22 20:24:12 -0500

    Quitando metodo main, al ser esta una libreria y no un proyecto a ejecutarse

--- src/main/java/com/lagm/springboot/app/commons/SpringbootServicioCommonsApplication.java
+++ src/main/java/com/lagm/springboot/app/commons/SpringbootServicioCommonsApplication.java
@@ -1,13 +1,7 @@
 package com.lagm.springboot.app.commons;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootServicioCommonsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootServicioCommonsApplication.class, args);
-	}
-
 }
